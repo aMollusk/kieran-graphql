031575dce3785729f0cce4706dc0587f988e435f
Kieran <kieran.mcdonald.sg@gmail.com>
2018-03-20 20:24:38 +1100

    initial commit

---
+++ src/main/java/com/kierangraphql/kieranGraphql/KieranGraphqlApplication.java
@@ -0,0 +1,12 @@
+package com.kierangraphql.kieranGraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieranGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieranGraphqlApplication.class, args);
+	}
+}
