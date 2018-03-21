09faa71eea316ed7f65726db9a5f21022112814d
Kieran <kieran.mcdonald.sg@gmail.com>
2018-03-21 20:25:50 +1100

    Inane comment

--- src/main/java/com/kierangraphql/kieranGraphql/KieranGraphqlApplication.java
+++ src/main/java/com/kierangraphql/kieranGraphql/KieranGraphqlApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KieranGraphqlApplication {
-
+	// This is the entry
 	public static void main(String[] args) {
 		SpringApplication.run(KieranGraphqlApplication.class, args);
 	}
